c53e9834aedda8b8c7d7402f0b4c28baab4d08b2
prabhatsih <prabhats1410@gmail.com>
2023-06-07 13:35:54 +0530

    JS_Day3_Variable

---
+++ assest/js/script.js
@@ -0,0 +1,54 @@
+//There are 4 way to declare variable in JS
+//1. Automatically
+//2. var
+//3. let
+//4. const
+
+var name = "Prabhat"; //Statement 1 Define & intialize the variable. 
+var surname = "Singh"; //Statement 2 Define & intialize the variable.
+var Mob = "91556....."; //Statement 2 Define & intialize the variable.
+
+console.log(name);
+console.log(surname);
+console.log(Mob);
+
+//a. Define a variable
+var fathername; //statement 1
+
+//b. Initialize the variable
+fathername = "ManjayLal singh"; //statement 2
+
+//c. excution
+console.log(fathername); //statement 3
+
+//a. Define a variable
+var mothername; //statement 1
+
+//b. Initialize the variable
+mothername = "Indu Devi"; //statement 2
+
+//c. Execution
+console.log(mothername); //statement 3
+
+//a. Define a variable using let keyword.
+let address;
+
+//b. Intialize a variable
+address = "Samastipur, Bihar, India"
+
+//c. execution
+console.log(address);
+
+//a. Define & Intialize the variable using let keyword.
+let college = "BRB College Samastipur";
+
+//b. Execution
+console.log(college);
+
+//a. Define & Intialize the variable using const keyword.
+const frnd1 = "Aditya";
+const frnd2 = "Nitish";
+
+//b. execution
+console.log(frnd1);
+console.log(frnd2);
